855971ec98d115948ff0c7b6f8f3e630dbafba5d
ega-jismi <egajismisaputra@gmail.com>
2025-11-26 21:47:26 +0700

    ubah tampilan

--- project-web/store/store.js
+++ project-web/store/store.js
@@ -1,41 +1,42 @@
 import { create } from 'zustand'
 
 const useStore = create((set) => ({
+  // --- STATE GLOBAL ---
   cart: [],
   wishlist: [],
   query: '',
+  selectedCategory: 'All',
+  isLoggedIn: false, // <--- STATE LOGIN BARU
   
-  // Mengubah query pencarian
+  // --- ACTIONS ---
   setQuery: (q) => set({ query: q }),
+  setSelectedCategory: (category) => set({ selectedCategory: category }),
+  
+  // Fungsi mengubah status login (Dipanggil saat login/logout/cek cookie)
+  setLoggedIn: (status) => set({ isLoggedIn: status }),
 
-  // Menambah buku ke keranjang
   addToCart: (book) => set((state) => {
     const exists = state.cart.find((i) => i.id === book.id)
     if (exists) {
-      return {
-        cart: state.cart.map((i) =>
-          i.id === book.id ? { ...i, qty: i.qty + 1 } : i
-        ),
-      }
+      return { cart: state.cart.map((i) => i.id === book.id ? { ...i, qty: i.qty + 1 } : i) }
     }
     return { cart: [...state.cart, { ...book, qty: 1 }] }
   }),
 
-  // Menghapus 1 item dari keranjang
-  removeFromCart: (id) =>
-    set((state) => ({ cart: state.cart.filter((i) => i.id !== id) })),
+  decreaseQty: (id) => set((state) => {
+    const item = state.cart.find((i) => i.id === id);
+    if (item.qty > 1) {
+        return { cart: state.cart.map((i) => i.id === id ? { ...i, qty: i.qty - 1 } : i) }
+    } else {
+        return { cart: state.cart.filter((i) => i.id !== id) } 
+    }
+  }),
 
-  // --- FUNGSI BARU: KOSONGKAN KERANJANG ---
-  // Dipanggil saat pembayaran sukses
+  removeFromCart: (id) => set((state) => ({ cart: state.cart.filter((i) => i.id !== id) })),
   clearCart: () => set({ cart: [] }), 
-  // ----------------------------------------
 
-  // Menambah/Hapus Wishlist
-  toggleWishlist: (id) =>
-    set((state) => {
-      if (state.wishlist.includes(id)) {
-        return { wishlist: state.wishlist.filter((x) => x !== id) }
-      }
+  toggleWishlist: (id) => set((state) => {
+      if (state.wishlist.includes(id)) { return { wishlist: state.wishlist.filter((x) => x !== id) } }
       return { wishlist: [...state.wishlist, id] }
     }),
 }))
