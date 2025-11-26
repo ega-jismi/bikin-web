e15f30b91e3f7b6fca77f256046dced97ede4df1
andika121103 <dikasihombing44@gmail.com>
2025-11-26 12:52:16 +0700

    Refactor store.js and add clearCart functionality
    Refactor store functions for clarity and add clearCart function.

--- project-web/store/store.js
+++ project-web/store/store.js
@@ -1,20 +1,43 @@
-import {create} from 'zustand'
+import { create } from 'zustand'
 
-const useStore = create(set => ({
+const useStore = create((set) => ({
   cart: [],
   wishlist: [],
   query: '',
-  setQuery: (q)=> set({ query: q }),
-  addToCart: (book) => set(state => {
-    const exists = state.cart.find(i=>i.id===book.id)
-    if(exists) return { cart: state.cart.map(i => i.id===book.id ? { ...i, qty: i.qty+1 } : i) }
+  
+  // Mengubah query pencarian
+  setQuery: (q) => set({ query: q }),
+
+  // Menambah buku ke keranjang
+  addToCart: (book) => set((state) => {
+    const exists = state.cart.find((i) => i.id === book.id)
+    if (exists) {
+      return {
+        cart: state.cart.map((i) =>
+          i.id === book.id ? { ...i, qty: i.qty + 1 } : i
+        ),
+      }
+    }
     return { cart: [...state.cart, { ...book, qty: 1 }] }
   }),
-  removeFromCart: (id) => set(state => ({ cart: state.cart.filter(i=>i.id!==id) })),
-  toggleWishlist: (id) => set(state => {
-    if(state.wishlist.includes(id)) return { wishlist: state.wishlist.filter(x=>x!==id) }
-    return { wishlist: [...state.wishlist, id] }
-  })
+
+  // Menghapus 1 item dari keranjang
+  removeFromCart: (id) =>
+    set((state) => ({ cart: state.cart.filter((i) => i.id !== id) })),
+
+  // --- FUNGSI BARU: KOSONGKAN KERANJANG ---
+  // Dipanggil saat pembayaran sukses
+  clearCart: () => set({ cart: [] }), 
+  // ----------------------------------------
+
+  // Menambah/Hapus Wishlist
+  toggleWishlist: (id) =>
+    set((state) => {
+      if (state.wishlist.includes(id)) {
+        return { wishlist: state.wishlist.filter((x) => x !== id) }
+      }
+      return { wishlist: [...state.wishlist, id] }
+    }),
 }))
 
 export default useStore
