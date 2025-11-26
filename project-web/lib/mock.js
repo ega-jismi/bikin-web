e28a2777b2401b02f41abe032bc4f6dde0e2656f
ega-jismi <egajismisaputra@gmail.com>
2025-11-26 14:13:34 +0700

    Ubah Gambar

--- project-web/lib/mock.js
+++ project-web/lib/mock.js
@@ -4,7 +4,7 @@ export const books = [
     title: 'How Innovation Works',
     author: 'Ega Jismina',
     price: 120000,
-    cover: '/images/book1.avif',
+    cover: 'https://cdn.gramedia.com/uploads/picture_meta/2023/5/26/uwuojnsnsdhxfgtaztmu7x.jpg',
     tags: ['programming','web'],
     description: 'Pengantar lengkap pemrograman web: HTML, CSS, JavaScript, dan praktik dasar.'
   },
@@ -13,7 +13,7 @@ export const books = [
     title: 'Zero to One',
     author: 'Sinta Pratiwi',
     price: 150000,
-    cover: '/images/book2.avif',
+    cover: 'https://gpu.id/data-gpu/images/uploads/book/6cb2696cebc9dee871f57063ba1290b7.jpg',
     tags: ['react','frontend'],
     description: 'Panduan praktis membangun aplikasi modern dengan React dan ekosistemnya.'
   },
@@ -22,7 +22,7 @@ export const books = [
     title: 'Psychology of Money',
     author: 'Budi Hartono',
     price: 90000,
-    cover: '/images/book3.avif',
+    cover: 'https://cdn.gramedia.com/uploads/picture_meta/2024/1/20/qvjtc65vbzmexfegzrgs7u.jpg',
     tags: ['database','sql'],
     description: 'Dasar-dasar database relasional, query SQL, dan desain skema.'
   },
@@ -31,7 +31,7 @@ export const books = [
     title: 'Milk and Honey',
     author: 'Citra Lestari',
     price: 200000,
-    cover: '/images/book4.avif',
+    cover: 'https://www.gramedia.com/blog/content/images/2022/05/milk-and-honey.jpg',
     tags: ['algorithms','interview'],
     description: 'Referensi algoritma dan struktur data untuk studi dan wawancara teknis.'
   },
@@ -40,7 +40,7 @@ export const books = [
     title: 'Desain UI/UX untuk Web',
     author: 'Rizal Kurnia',
     price: 110000,
-    cover: '/images/book5.avif',
+    cover: 'https://image.gramedia.net/rs:fit:0:0/plain/https://cdn.gramedia.com/uploads/products/-391t2araa.jpg',
     tags: ['design','uiux'],
     description: 'Prinsip desain antarmuka dan pengalaman pengguna untuk web modern.'
   }
